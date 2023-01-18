af542864c6ae0f5c1acdbcd5551650f50bc50c66
Seme87 <massimomonteleone@hotmail.com>
2023-01-18 10:45:42 +0100

    Utilizza nella query la funzione carbon

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,13 +5,13 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use App\Models\Train;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Carbon;
 
 class PageController extends Controller
 {
     public function index()
     {
-        $trains = Train::where('departure_time','2023-01-17 06:00:00')->get(); 
+        $trains = Train::where('departure_time' >= Carbon::now())->get(); 
 
         
        return view('homepage', compact('trains'));
